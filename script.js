7d7f817a023666ca3b2ae35f7e02cabc79a6246a
Yah'Mir McGhee <yahmir.mcghee@gmail.com>
2021-10-11 18:15:15 -0400

    Timeblocks, Description Rectangles, and Save Buttons for each has been Created

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 console.log ("Am I online?")
-var today= moment();
-$("#currentday").text(today.format("MM Do, YYYY"));
+
+
+var currDay= moment().format('dddd');
+var currDate= moment().format("MMM Do YY");
+var currHour=moment().format('LT');
+$("#currentDay").append(currDay+ " "+ currDate+" "+ currHour)
+
+
+$(function() { 
+    $("button.savebutton").on("click", function(event) {
+        console.log("Button clicked: ", event.target);
+    });
+  });
+
+// Need Eventlistener
