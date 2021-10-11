a41483b40a08eefac23ebd5ca151300866125ca9
Yah'Mir McGhee <yahmir.mcghee@gmail.com>
2021-10-11 18:18:56 -0400

    Current Date and Allowing Computer to Identify Button(s) Clicked Has Been Created

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log ("Am I online?")
 
-
+//How to Set Current Date and Format
 var currDay= moment().format('dddd');
 var currDate= moment().format("MMM Do YY");
 var currHour=moment().format('LT');
 $("#currentDay").append(currDay+ " "+ currDate+" "+ currHour)
 
-
+// How to Allow Save Button to Click And Allow Computer to Identify Button(s) Clicked
 $(function() { 
     $("button.savebutton").on("click", function(event) {
         console.log("Button clicked: ", event.target);
